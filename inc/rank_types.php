ea1773859e5c448fd65a07edf10e6e5b75cc2795
Xtreme Webdesign <shinylilligantt@gmail.com>
2016-03-02 15:00:22 -0300

    Cleaning up the code

--- inc/rank_types.php
+++ inc/rank_types.php
@@ -1,8 +1,8 @@
 <?php
-	$url = explode("/", $_SERVER["REQUEST_URI"]);
+    $url = explode("/", $_SERVER["REQUEST_URI"]);
     if(isset($url[1])){
     	if(is_numeric($url[1]) && $url[1] > -1 && $url[1] < 3){
-			$ranktype = $url[1];
+		$ranktype = $url[1];
     	}
     	else{
     		$ranktype = 0;
